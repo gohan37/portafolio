d1861285d2270a97494be31f450b2534764662dd
nexcodign <nachouema8567@gmail.com>
2025-11-29 10:10:13 -0300

    Cambios: base Vite y router para GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { createRoot } from 'react-dom/client'
+import { HashRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(
+createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router>
+    <HashRouter>
       <App />
-    </Router>
+    </HashRouter>
   </React.StrictMode>
 )
